abdb01da74a6ce853dffff8634de0f2d5ac06bb8
Luke Granger <grangerl330@gmail.com>
2019-01-09 04:44:25 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -27,7 +27,12 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  i = 0 
+  data.each do |season_number, contestant_array|
+    while i < contestant_array.length 
+      contestant_array.each do |contestant_hash|
+        if 
+       
 end
 
 def get_occupation(data, hometown)
