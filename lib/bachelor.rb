159fa6603d1818c8949b1d60158443c3c022b2fd
Luke Granger <grangerl330@gmail.com>
2019-01-09 03:58:58 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,19 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  season_winner = ""
+  winner_array = []
+  data.each do |season_number, contestant_array|
+    if season_number == season 
+      contestant_array.each do |contestant_hash|
+        if contestant_hash["status"] == "Winner"
+          season_winner = contestant_hash["name"]
+          winner_array = season_winner.split
+        end
+      end
+    end
+  end
+  winner_array[0]
 end
 
 def get_contestant_name(data, occupation)
